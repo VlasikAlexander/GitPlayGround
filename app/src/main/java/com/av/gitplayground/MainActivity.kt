6bfdbeb872c3066e3a771511eded84f7ffc80478
alexander.vlasik <alexander.vlasik@anagog.com>
2023-02-01 14:39:38 +0200

    local commit

--- app/src/main/java/com/av/gitplayground/MainActivity.kt
+++ app/src/main/java/com/av/gitplayground/MainActivity.kt
@@ -17,25 +17,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GitPlayGroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                    Greeting(name = "New name")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GitPlayGroundTheme {
-        Greeting("Android")
-    }
-}
